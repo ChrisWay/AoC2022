892b4813526d7a25602c0cc35183c1b2177cb626
Chris Way <644592+ChrisWay@users.noreply.github.com>
2022-12-01 21:41:18 +0000

    Remove duplicate code

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -11,26 +11,24 @@ public class Day01 : BaseDay
 
     public override ValueTask<string> Solve_1()
     {
-        var lines = _input.Split(Environment.NewLine, StringSplitOptions.None);
-        var totals = new List<int>();
-        int runningTotal = 0;
-
-        foreach (var line in lines)
-        {
-            if (string.IsNullOrEmpty(line))
-            {
-                totals.Add(runningTotal);
-                runningTotal = 0;
-                continue;
-            }
-
-            runningTotal += int.Parse(line);
-        }
+        var totals = GetTotals();
 
         return ValueTask.FromResult(totals.Max().ToString());
     }
 
     public override ValueTask<string> Solve_2()
+    {
+        var totals = GetTotals();
+
+        var topThreeTotal = totals
+            .OrderByDescending(x => x)
+            .Take(3)
+            .Sum();
+
+        return ValueTask.FromResult(topThreeTotal.ToString());
+    }
+
+    List<int> GetTotals()
     {
         var lines = _input.Split(Environment.NewLine, StringSplitOptions.None);
         var totals = new List<int>();
@@ -48,11 +46,6 @@ public class Day01 : BaseDay
             runningTotal += int.Parse(line);
         }
 
-        var topThreeTotal = totals
-            .OrderByDescending(x => x)
-            .Take(3)
-            .Sum();
-
-        return ValueTask.FromResult(topThreeTotal.ToString());
+        return totals;
     }
 }
