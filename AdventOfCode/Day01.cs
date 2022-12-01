518e1b9060e4bac2c5b0b5bf04c9e4e164023c04
Chris Way <644592+ChrisWay@users.noreply.github.com>
2022-12-01 15:33:05 +0000

    Add both solutions for day 1

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -9,7 +9,50 @@ public class Day01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
 
-    public override ValueTask<string> Solve_1() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 1");
+    public override ValueTask<string> Solve_1()
+    {
+        var lines = _input.Split(Environment.NewLine, StringSplitOptions.None);
+        var totals = new List<int>();
+        int runningTotal = 0;
 
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                totals.Add(runningTotal);
+                runningTotal = 0;
+                continue;
+            }
+
+            runningTotal += int.Parse(line);
+        }
+
+        return ValueTask.FromResult(totals.Max().ToString());
+    }
+
+    public override ValueTask<string> Solve_2()
+    {
+        var lines = _input.Split(Environment.NewLine, StringSplitOptions.None);
+        var totals = new List<int>();
+        int runningTotal = 0;
+
+        foreach (var line in lines)
+        {
+            if (string.IsNullOrEmpty(line))
+            {
+                totals.Add(runningTotal);
+                runningTotal = 0;
+                continue;
+            }
+
+            runningTotal += int.Parse(line);
+        }
+
+        var topThreeTotal = totals
+            .OrderByDescending(x => x)
+            .Take(3)
+            .Sum();
+
+        return ValueTask.FromResult(topThreeTotal.ToString());
+    }
 }
